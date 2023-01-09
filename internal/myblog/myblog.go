16989bd1f91b7c4144aaae30dc759e452ece4f39
kiyonamiy <yuqingbo0122@gmail.com>
2023-01-09 23:49:33 +0800

    feat(*): add https and cors

--- internal/myblog/myblog.go
+++ internal/myblog/myblog.go
@@ -6,11 +6,20 @@
 package myblog
 
 import (
-	"encoding/json"
+	"context"
+	"errors"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/kiyonamiy/myblog/internal/pkg/log"
+	mw "github.com/kiyonamiy/myblog/internal/pkg/middleware"
 	"github.com/kiyonamiy/myblog/pkg/version/verflag"
+
+	"github.com/gin-gonic/gin"
 	"github.com/kjzz/viper"
 	"github.com/spf13/cobra"
 )
@@ -62,12 +71,107 @@ https://github.com/kiyonamiy/myblog`,
 	return cmd
 }
 
-// run 函数是实际的业务代码入口函数.
+// run 函数是实际的业务代码入口函数。
 func run() error {
-	// 打印所有的配置项及其值
-	settings, _ := json.Marshal(viper.AllSettings())
-	log.Infow(string(settings))
-	// 打印 db -> username 配置项的值
-	log.Infow(viper.GetString("db.username"))
+	// 设置 Gin 模式
+	gin.SetMode(viper.GetString("runmode"))
+
+	// 创建 Gin 引擎
+	g := gin.New()
+
+	// gin.Recovery() 中间件，用来捕获任何 panic，并恢复
+	mws := []gin.HandlerFunc{gin.Recovery(), mw.NoCache, mw.Cors, mw.Secure}
+
+	g.Use(mws...)
+
+	// 注册 404 Handler
+	g.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"code": 10003, "message": "Page not found."})
+	})
+
+	// 注册 /healthz handler
+	g.GET("/healthz", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	g.LoadHTMLGlob("web/*")
+	g.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "我是测试", "ce": "123456"})
+	})
+
+	// // 创建 HTTP Server 实例
+	// httpsrv := &http.Server{Addr: viper.GetString("addr"), Handler: g}
+	// if err := httpsrv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+	// 	log.Fatalw(err.Error())
+	// }
+
+	// 创建并运行 HTTP 服务器
+	httpsrv := startInsecureServer(g)
+
+	// 创建并运行 HTTPS 服务器
+	httpssrv := startSecureServer(g)
+
+	// 等待中断信号优雅地关闭服务器（10 秒超时)。
+	//quit := make(chan os.Signal, 1)
+	quit := make(chan os.Signal)
+	// kill 默认会发送 syscall.SIGTERM 信号
+	// kill -2 发送 syscall.SIGINT 信号，我们常用的 CTRL + C 就是触发系统 SIGINT 信号
+	// kill -9 发送 syscall.SIGKILL 信号，但是不能被捕获，所以不需要添加它
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM) // 此处不会阻塞
+	<-quit                                               // 阻塞在此，当接收到上述两种信号时才会往下执行
+	log.Infow("Shutting down server ...")
+
+	// 创建 ctx 用于通知服务器 goroutine, 它有 10 秒时间完成当前正在处理的请求
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// 10 秒内优雅关闭服务（将未处理完的请求处理完再关闭服务），超过 10 秒就超时退出
+	if err := httpsrv.Shutdown(ctx); err != nil {
+		log.Errorw("Insecure Server forced to shutdown", "err", err)
+		return err
+	}
+	if err := httpssrv.Shutdown(ctx); err != nil {
+		log.Errorw("Secure Server forced to shutdown", "err", err)
+		return err
+	}
+
+	log.Infow("Server exiting")
+
 	return nil
+}
+
+// startInsecureServer 创建并运行 HTTP 服务器
+func startInsecureServer(g *gin.Engine) *http.Server {
+	// 创建 HTTP Server 实例
+	httpsrv := &http.Server{Addr: viper.GetString("addr"), Handler: g}
+
+	// 运行 HTTP 服务器 // 在 goroutine 中启动服务器，它不会阻止下面的正常关闭处理流程
+	// 打印一条日志，用来提示 HTTP 服务已经起来，方便排障
+	log.Infow("Start to listening the incoming requests on http address", "addr", viper.GetString("addr"))
+	go func() {
+		if err := httpsrv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalw(err.Error())
+		}
+	}()
+
+	return httpsrv
+}
+
+func startSecureServer(g *gin.Engine) *http.Server {
+	// 创建 HTTPS Server 实例
+	httpssrv := &http.Server{Addr: viper.GetString("tls.addr"), Handler: g}
+
+	// 运行 HTTPS 服务器。在 goroutine 中启动服务器，它不会阻止下面的正常关闭处理流程
+	// 打印一条日志，用来提示 HTTPS 服务已经起来，方便排障
+	log.Infow("Start to listening the incoming requests on https address", "addr", viper.GetString("tls.addr"))
+	cert, key := viper.GetString("tls.cert"), viper.GetString("tls.key")
+	if cert != "" && key != "" {
+		go func() {
+			if err := httpssrv.ListenAndServeTLS(cert, key); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				log.Fatalw(err.Error())
+			}
+		}()
+	}
+
+	return httpssrv
 }
